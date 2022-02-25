372d4f3dba8fa7798c34629dedb1362dc8970b9a
Pieter Staal <pieterstaal@outlook.com>
2022-02-25 18:17:27 +0100

    Add tests for simulate(Manager|Subordinate)

--- SimulateTest.hs
+++ SimulateTest.hs
@@ -15,3 +15,34 @@ sim = uncurry L.zip $ simulateC' def
   [Protocols.Df.Data 1, Protocols.Df.Data 2, NoData, NoData, NoData, NoData]
   [Ack False, Ack False, Ack False, Ack False, Ack False, Ack True, Ack False, Ack True]
   (withClockResetEnable clockGen resetGen enableGen $ circuit' @System)
+
+simMan = simulateManager def
+  (Ack <$> [True, True, False, False, False, False, True])
+  (withClockResetEnable clockGen resetGen enableGen $ circ @System)
+  where
+    circ :: HiddenClockResetEnable dom =>
+      Circuit () (Df dom Int)
+    circ = (drive def [Data 1, Data 2, Data 3]) |> registerFwd
+
+simSub = simulateSubordinate def
+  ([NoData, Data 10, NoData, NoData, Data 5])
+  (withClockResetEnable clockGen resetGen enableGen $ circ @System)
+  where
+    circ :: HiddenClockResetEnable dom =>
+      Circuit (Df dom Int) ()
+    circ = Circuit go
+      where
+        go (dataSig, ()) = (ackSig, ())
+          where
+            ackSig = (\r -> Ack $ r <= 1) <$> regSig
+
+            regSig = register 4 setSig
+
+            setSig = setter <$> dataSig <*> regSig
+            setter d current = if current == 0
+              then case d of
+                Data n -> n
+                NoData -> current
+              else current - 1
+
+
