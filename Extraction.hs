6191ca158d6e9f771d51c11df44563cb27425bb4
Pieter Staal <pieterstaal@outlook.com>
2022-03-07 12:26:16 +0100

    Add extraction file

---
+++ Extraction.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE LambdaCase #-}
+module Extraction where
+
+import Clash.Prelude
+import Protocols
+import Protocols.Axi4.Common
+import Protocols.Axi4.Lite.Axi4Lite
+import Protocols.Internal
+import Data.List as L
